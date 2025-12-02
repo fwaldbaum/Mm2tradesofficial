d3239b77cc51648efe9fa008060b39bff4338c53
fwaldbaum <waldbaumfacundo@gmail.com>
2025-12-02 20:57:30 -0300

    Update response messages in buttonHandler.js

--- buttons/buttonHandler.js
+++ buttons/buttonHandler.js
@@ -1,9 +1,9 @@
 export default async function (interaction) {
   if (interaction.customId === "confirm_mm2") {
-    return interaction.reply("🔗 Un trader te enviará un servidor privado MM2.");
+    return interaction.reply("🔗 Un trader te enviará un servidor privado de MM2.");
   }
 
   if (interaction.customId === "confirm_sab") {
-    return interaction.reply("🧠 Un trader te enviará un servidor de SAB.");
+    return interaction.reply("🧠 Un trader te enviará un server de SAB.");
   }
 }
