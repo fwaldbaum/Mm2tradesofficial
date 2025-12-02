82bfc1d109c8ba5726147f51f7501d77f06630e3
fwaldbaum <waldbaumfacundo@gmail.com>
2025-12-02 20:55:31 -0300

    Update interaction reply messages for clarity

--- menus/selectMenuHandler.js
+++ menus/selectMenuHandler.js
@@ -2,10 +2,10 @@ export default async function (interaction) {
   const value = interaction.values[0];
 
   if (value === "mm2") {
-    await interaction.reply("🗡 Escribe `mm2: tus armas` para calcular precio.");
+    await interaction.reply("🗡 Escribe: `mm2: arma1, arma2, arma3` para calcular valores.");
   }
 
   if (value === "sab") {
-    await interaction.reply("🧠 Escribe `sab: tu brainrot` para contactar a un trader.");
+    await interaction.reply("🧠 Escribe: `sab: brainrot` para iniciar trade.");
   }
 }
